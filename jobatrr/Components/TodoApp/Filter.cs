e9de3193bcea5a4f087ccc7343355beb29993543
Edward Dikgale <edward.dikgale@gmail.com>
2023-04-26 18:36:01 +0200

    Initial Commit

---
+++ jobatrr/Components/TodoApp/Filter.cs
@@ -0,0 +1,8 @@
+﻿namespace jobatrr.Components.TodoApp;
+
+public enum Filter
+{
+    All,
+    Active,
+    Completed,
+}
---
+++ jobatrr/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+using Blazorise;
+using Blazorise.Material;
+using Blazorise.Icons.Material;
+
+namespace jobatrr
+{
+    public class Program
+    {
+        public static async Task Main( string[] args )
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault( args );
+            builder.RootComponents.Add<App>( "#app" );
+
+            builder.Services.AddScoped( sp => new HttpClient { BaseAddress = new Uri( builder.HostEnvironment.BaseAddress ) } );
+
+            AddBlazorise( builder.Services );
+
+            await builder.Build().RunAsync();
+        }
+
+        public static void AddBlazorise( IServiceCollection services )
+        {
+            services
+                .AddBlazorise();
+            services
+                .AddMaterialProviders()
+                .AddMaterialIcons();
+        }
+    }
+}
